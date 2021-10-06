530b5c77f11fef1778996c984a3263d0cd8cba81
Daniel <42664403+danieljimenez0255@users.noreply.github.com>
2021-10-05 23:22:06 -0700

    added reducers

--- app/store.js
+++ app/store.js
@@ -3,8 +3,14 @@
 */
 
 import { configureStore } from "@reduxjs/toolkit";
+import authCryptoReducer from "../Slice/authProfileSlice";
+import currencyCryptoReducer from "../Slice/CurrenciesSlice";
 import trackedCryptoReducer from "../Slice/trackedCryptoSlice";
 
 export default configureStore({
-  reducer: { crypto: trackedCryptoReducer },
+  reducer: {
+    crypto: trackedCryptoReducer,
+    auth: authCryptoReducer,
+    currency: currencyCryptoReducer,
+  },
 });
