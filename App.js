dc52fe8aa34c7ab269c215659452ccbca224d32f
Daniel <42664403+danieljimenez0255@users.noreply.github.com>
2021-10-05 23:09:50 -0700

    updated navigation

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-import React from "react";
-import { StyleSheet } from "react-native";
-import { createStackNavigator } from "@react-navigation/stack";
+import React, { useEffect } from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import LoginScreen from "./screen/LoginScreen";
 import RegisterScreen from "./screen/RegisterScreen";
@@ -9,9 +8,9 @@ import store from "./app/store";
 import { Provider } from "react-redux";
 import AddCryptoScreen from "./screen/AddCryptoScreen";
 
-export default function App() {
-  const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
+export default function App() {
   return (
     // Consists of the provider for redux and the Navigator for the stacked screens
     <Provider store={store}>
@@ -27,11 +26,11 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
 });
+ */
