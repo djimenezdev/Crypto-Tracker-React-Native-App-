4094387cf9697f09f74403bd15c6d0555025081a
Daniel <42664403+danieljimenez0255@users.noreply.github.com>
2021-10-05 23:21:30 -0700

    added curreny select fetch

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -4,23 +4,71 @@ Main Home Page UI that displays all tracked currencies
 
 */
 
-import React, { useLayoutEffect } from "react";
-import { TouchableOpacity } from "react-native";
-import { StyleSheet, View } from "react-native";
-import { Avatar, Text } from "react-native-elements";
-import { auth } from "../app/firebase";
-import { useSelector } from "react-redux";
-import { getSelectedCurrencies } from "../Slice/trackedCryptoSlice";
-import { ScrollView } from "react-native";
-import { SafeAreaView } from "react-native";
+import React, { useEffect, useLayoutEffect } from "react";
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  ScrollView,
+  SafeAreaView,
+  Text,
+} from "react-native";
+import { Avatar, Icon } from "react-native-elements";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  allCurrencyFetch,
+  getSelectedCurrencies,
+  clearCurrencies,
+} from "../Slice/trackedCryptoSlice";
 import TrackedList from "../components/TrackedList";
+import { clearInfo, getOtherInfo } from "../Slice/authProfileSlice";
+import { getAllCurrencies, getCurrencies } from "../Slice/CurrenciesSlice";
+import { auth, db } from "../app/firebase";
 
 const HomeScreen = ({ navigation }) => {
   const currencies = useSelector(getSelectedCurrencies);
+  const userLogin = useSelector(getOtherInfo);
+  const getAllCurrenciesM = useSelector(getAllCurrencies);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const checkUserData = async () => {
+      const checkData = db
+        ?.collection("userCurrencies")
+        ?.doc(`${userLogin?.userMeta?.email}`)
+        ?.collection("selectedCurrencies")
+        ?.get();
+      checkData?.then((querySnapshot) => {
+        if (querySnapshot?.docs?.length > 0) {
+          const allC = [];
+          querySnapshot.forEach((doc) => {
+            allC.push(doc.data());
+          });
+          dispatch(allCurrencyFetch({ all: allC }));
+        } else {
+          return;
+        }
+      });
+    };
+    checkUserData();
+  }, []);
+
+  useEffect(() => {
+    const fetchCurrencies = async () => {
+      const allCurrencies = await fetch(
+        "https://data.messari.io/api/v1/assets"
+      ).then((res) => res.json());
+      dispatch(getCurrencies({ currencies: allCurrencies }));
+    };
+    if (!getAllCurrenciesM?.currencies?.data) {
+      fetchCurrencies();
+      return;
+    }
+  }, [getAllCurrenciesM]);
+
   useLayoutEffect(() => {
     // Applies styling to header for home page
     navigation.setOptions({
-      title: "CryptoTracker - Home",
+      title: "Home",
       headerStyle: {
         backgroundColor: "#18a0fb",
         height: 100,
@@ -28,11 +76,31 @@ const HomeScreen = ({ navigation }) => {
       headerTitleStyle: {
         color: "#fff",
       },
-      headerTitleAlign: "left",
+      headerLeft: () => (
+        <TouchableOpacity
+          onPress={() => {
+            auth.signOut();
+            dispatch(clearCurrencies());
+            dispatch(clearInfo());
+            navigation.reset({
+              index: 0,
+              routes: [{ name: "Login" }],
+            });
+          }}
+        >
+          <Icon
+            name="logout"
+            color="white"
+            type="material"
+            size={24}
+            style={{ marginRight: 5 }}
+          />
+        </TouchableOpacity>
+      ),
       headerRight: () => (
-        <View style={{ marginRight: 20 }}>
+        <View style={{ marginRight: 10 }}>
           <TouchableOpacity activeOpacity={0.5}>
-            <Avatar rounded source={{ url: auth?.currentUser?.photoURL }} />
+            <Avatar rounded source={{ url: userLogin?.image }} />
           </TouchableOpacity>
         </View>
       ),
@@ -40,28 +108,25 @@ const HomeScreen = ({ navigation }) => {
   }, [navigation]);
   return (
     // Displays UI based on if there are any tracked Currencies or not
-    <SafeAreaView>
-      <ScrollView contentContainerStyle={styles.container}>
+
+    <ScrollView contentContainerStyle={styles.container}>
+      <SafeAreaView>
         {currencies?.length === 0 ? (
           <View>
-            <Text h3>
+            <Text h3 style={styles.containerHeader}>
               You haven't added a currency yet, click button below to do so!
             </Text>
           </View>
         ) : (
           <View>
-            {currencies?.map((currency) => (
-              <TrackedList
-                key={currency.data.name}
-                symbol={currency.data.symbol}
-                name={currency.data.name}
-                price={currency.data.market_data.price_usd}
-                percentage={
-                  currency.data.market_data.percent_change_usd_last_24_hours
-                }
-                currencies={currencies}
-              />
-            ))}
+            {currencies?.length > 0 &&
+              currencies?.map((currency, i) => (
+                <TrackedList
+                  key={currency?.symbol}
+                  currency={currency}
+                  currencies={currencies}
+                />
+              ))}
           </View>
         )}
         <Text
@@ -71,8 +136,8 @@ const HomeScreen = ({ navigation }) => {
         >
           + Add a Cryptocurrency
         </Text>
-      </ScrollView>
-    </SafeAreaView>
+      </SafeAreaView>
+    </ScrollView>
   );
 };
 
@@ -80,10 +145,21 @@ export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
     alignItems: "center",
+  },
+  containerHeader: {
+    fontSize: 20,
+    marginTop: 10,
   },
   addCryptoText: {
     marginTop: 10,
     color: "turquoise",
+    fontSize: 25,
+    textAlign: "center",
   },
 });
